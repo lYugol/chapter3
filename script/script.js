511ac621c5a316498f7f2424e641c3b3eb215db0
lYugol <156240240+lYugol@users.noreply.github.com>
2024-03-28 15:39:28 +0300

    3

--- script/script.js
+++ script/script.js
@@ -20,12 +20,12 @@ const swiper = new Swiper('.sample-slider', {
 })
 
 function show() {
-    const assideButtonOn = document.getElementById('on');
+    const assideButtonOn = document.querySelector('.aside__button-on');
     const swiperSlides = document.querySelectorAll('.swiper-slide');
 
     assideButtonOn.addEventListener('click', function(){
         this.classList.toggle('swap');
-        if (assideButtonOn.textContent === 'Показать всё') {
+        if (assideButtonOn.classList.contains('swap')) {
             assideButtonOn.textContent = 'Скрыть';
             for (let i = 0; i < swiperSlides.length; i++) {
             swiperSlides[i].classList.add('visible');
